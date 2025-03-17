16d6b3123b929d3b429dd3be8dae77c3c61debcc
felipe@augusto.com <felipeaugustoluf@gmail.com>
2025-03-16 22:13:34 -0300

    CrCreating the menu screen and adding the other folders

---
+++ code/Menu.py
@@ -0,0 +1,38 @@
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WIDTH, COLOR_DARK, MENU_OPTION, COLOR_WHITE
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./assets/Menu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self):
+        pygame.mixer_music.load('./assets/menu.wav')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            self.window.blit(self.surf, self.rect)
+            self.menu_text(50, "Dark", COLOR_DARK, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "City", COLOR_DARK, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Verifica eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
---
+++ code/const.py
@@ -0,0 +1,13 @@
+# c
+COLOR_DARK = (5, 5, 5)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COORPERATIVE',
+               'NEW GAME 2P - COORPERATIVE',
+               'SCORE',
+               'EXITE')
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
